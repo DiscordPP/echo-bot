578b213e99f72c5577042158507d551d6a89ae9a
Unknown <Aidan.S.Edwards@gmail.com>
2017-07-16 12:22:01 -0500

    Compiles, but not done yet.

--- main.cc
+++ main.cc
@@ -1,6 +1,54 @@
 #include <iostream>
+#include <boost/filesystem.hpp>
+#include <discordpp/discordpp.hh>
+#include <discordpp/rest-curlpp.hh>
+#include <discordpp/websocket-websocketpp.hh>
+
+#include <lib/nlohmannjson/src/json.hpp>
+
+std::string readTokenFile(std::string tokenFilePath);
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Starting bot...\n\n";
+
+    /*/
+     * Read token from token file.
+     * Tokens are required to communicate with Discord, and hardcoding tokens is a bad idea.
+     * If your bot is open source, make sure it's ignore by git in your .gitignore file.
+    /*/
+    std::string token;
+    if(boost::filesystem::exists("token.dat")){
+        token = readTokenFile("token.dat");
+    } else {
+        std::cerr << "CRITICAL: There is no valid way for Discord++ to obtain a token! Copy the example login.dat or token.dat to make one.\n";
+        exit(1);
+    }
+
+    discordpp::Bot bot(
+            token,
+            std::make_shared<discordpp::RestCurlPPModule>(),
+            std::make_shared<discordpp::WebsocketWebsocketPPModule>()
+    );
+
+
+
     return 0;
+}
+
+std::string readTokenFile(std::string tokenFilePath){
+    std::ifstream tokenFile;
+    tokenFile.open(tokenFilePath);
+
+    std::string token;
+
+    if (tokenFile.is_open()) {
+        std::getline(tokenFile, token);
+    } else {
+        std::cerr << "CRITICAL: There is no such file as " + tokenFilePath + "! Copy the example login.dat to make one.\n";
+        exit(1);
+    }
+    tokenFile.close();
+    std::cout << "Retrieved token.\n\n";
+
+    return token;
 }

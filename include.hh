820c2a6b563ccdd67819d0808aae870977b3ddf7
Aidan Edwards <Aidan.S.Edwards@gmail.com>
2021-09-27 19:44:02 -0500

    Easier plugin inclusion

--- include.hh
+++ include.hh
@@ -4,6 +4,20 @@
 
 #pragma once
 
+#include <discordpp/macros.hh>
+
+#define BOT_DEF_OPEN(plugin) plugin <
+#define BOT_DEF_CLOSE(plugin) >
+#define BOT_DEF(...)                                                           \
+    DPP_FOR_EACH(BOT_DEF_OPEN, __VA_ARGS__)                                    \
+    discordpp::Bot DPP_FOR_EACH(BOT_DEF_CLOSE, __VA_ARGS__)
+
+#define MY_BOT_DEF                                                             \
+    BOT_DEF(discordpp::PluginInteractionHandler, discordpp::PluginEndpoints,   \
+            discordpp::PluginResponder, discordpp::PluginOverload,             \
+            discordpp::PluginRateLimit, discordpp::WebsocketSimpleWeb,         \
+            discordpp::RestBeast)
+
 #ifndef ECHO_EXTERN
 
 #include <fstream>
@@ -17,11 +31,11 @@
 #endif
 
 #include <discordpp/bot.hh>
+#include <discordpp/plugin-endpoints.hh>
+#include <discordpp/plugin-interactionhandler.hh>
 #include <discordpp/plugin-overload.hh>
 #include <discordpp/plugin-ratelimit.hh>
 #include <discordpp/plugin-responder.hh>
-#include <discordpp/plugin-endpoints.hh>
-#include <discordpp/plugin-interactionhandler.hh>
 #include <discordpp/rest-beast.hh>
 #include <discordpp/websocket-simpleweb.hh>
 
@@ -29,8 +43,8 @@
 
 #ifndef ECHO_EXTERN
 
-extern template class discordpp::PluginInteractionHandler<discordpp::PluginEndpoints<discordpp::PluginResponder<discordpp::PluginOverload<discordpp::PluginRateLimit<discordpp::WebsocketSimpleWeb<discordpp::RestBeast<discordpp::Bot>>>>>>>;
-using DppBot = discordpp::PluginInteractionHandler<discordpp::PluginEndpoints<discordpp::PluginResponder<discordpp::PluginOverload<discordpp::PluginRateLimit<discordpp::WebsocketSimpleWeb<discordpp::RestBeast<discordpp::Bot>>>>>>>;
+extern template class MY_BOT_DEF;
+using DppBot = MY_BOT_DEF;
 
 #undef DPPBOTDEF
 
